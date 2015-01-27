014afda28aeff63282839e64fa7d9efa4058f9cc
Lars Kuhtz <lars@alephcloud.com>
2015-01-26 17:54:46 -0800

    Add Timeout.hs and Timeout.html

---
+++ test/Timeout.hs
@@ -0,0 +1,117 @@
+-- |
+-- Module: Main
+-- Description: benchmarks for concurrent use of 'timeout'
+-- Maintainer: Lars Kuhtz <lkuhtz@pivotmail.com>
+--
+-- These benchmarks evaluate the performance of 'timeout' when used for many
+-- short-running sequential actions from a moderate number of concurrent
+-- threads.
+--
+-- In concrete the benchmarks spawn 16 concurrent threads and have each
+-- sequentially repeat the same action 1000 times. The action consists @n@ of
+-- sequential calls to 'getCurrentTime'. The benchmarks are executed for
+-- different values for @n@.
+--
+-- We compare are four types of benchmarks:
+--
+-- 1. Just execute the setup described above as is.
+--
+-- 2. Wraps each action of @n@ calls to 'getCurrentTime' into a call to
+--    'timeout'. The timeout parameter is chosen large enought so that it will
+--    trigger.
+--
+-- 3. Each action of @n@ calls to 'getCurrentTime' is started
+--    only after updating an 'MVar' that is shared by all threads. The update
+--    is a cheap, but not yet trivial pure function.
+--
+-- 4. Each action of @n@ calls to 'getCurrentTime' is started only atomically
+--    updating an 'IORef' that is shared by all threads. The update is a cheap,
+--    but not yet trivial pure function.
+--
+-- Note that the third and the fourth scenario force a synchronization between
+-- *all* threads, whereas, to the best of my knowledge, the call to timeout
+-- only requires synchronization between all threads on the same capability.
+--
+module Main
+( main
+) where
+
+import Control.Concurrent.MVar (modifyMVar_, newMVar, newEmptyMVar, putMVar, takeMVar)
+import Control.Concurrent (forkIO)
+import Control.Exception (finally)
+import Control.Monad (replicateM, replicateM_)
+import Data.IORef (newIORef, atomicModifyIORef')
+import Data.Time.Clock (getCurrentTime)
+import System.Timeout (timeout)
+import Criterion.Main (bgroup, bench, defaultMain, nfIO)
+
+threadN :: Int
+threadN = 16
+
+main :: IO ()
+main = defaultMain $
+    [ benchmark "plain" run1
+    , benchmark "with-timeout" run2
+    , benchmark "with-mvar" run3
+    , benchmark "with-ioref" run4
+    ]
+  where
+    params = [5,10,50,100,500,1000]
+    benchmark label action = bgroup label
+        [ bench (show i) . nfIO $ action i | i <- params ]
+
+-- | Run 'threadN' threads which each call 'getCurrentTime'
+-- 1000 times.
+--
+run1 :: Int -> IO ()
+run1 n = forkN threadN $
+    replicateM_ 1000 . replicateM_ n $ getCurrentTime
+
+-- | Run 'threadN' threads which each call 'getCurrentTime'
+-- 1000 times with a timeout of 10 seconds (which never triggers).
+--
+run2 :: Int -> IO ()
+run2 n = forkN threadN $
+    replicateM_ 1000 . timeout 10000000 . replicateM_ n $ getCurrentTime
+
+-- | All threads share an 'MVar' that each thread updates
+-- 1000 times.
+--
+run3 :: Int -> IO ()
+run3 n = do
+    mvar <- newMVar [0 :: Int]
+    forkN threadN $
+        replicateM_ 1000 $ do
+            modifyMVar_ mvar (return . func)
+            replicateM_ n $ getCurrentTime
+
+-- | All threads share an 'IOref' that each thread updates
+-- 1000 times.
+--
+run4 :: Int -> IO ()
+run4 n = do
+    var <- newIORef [0 :: Int]
+    forkN threadN $
+        replicateM_ 1000 $ do
+            atomicModifyIORef' var (\a -> (func a, ()))
+            replicateM_ n $ getCurrentTime
+
+-- | Some pure function, that is not completely trivial
+-- but yet cheap.
+--
+func :: [Int] -> [Int]
+func a = take 40 $ a ++ [0]
+
+-- | fork n processes and let them do some work in IO
+--
+forkN :: Int -> IO () -> IO ()
+forkN n f = do
+    vars <- replicateM n $ do
+        v <- newEmptyMVar
+        _ <- forkIO $ f `finally` putMVar v ()
+        return v
+    mapM_ takeMVar vars
+
+module Timeout
+(
+) where
